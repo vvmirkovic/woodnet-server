1069c7d10cf724ced7be4d54c61402bd40a76be2
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-03-11 01:14:29 -0500

    correct zone name

--- modules/frontend/route53.tf
+++ modules/frontend/route53.tf
@@ -53,34 +53,38 @@ resource "aws_route53_record" "woodnet_domain" {
   }
 }
 
-# resource "aws_acm_certificate" "woodnet" {
-#   domain_name       = "*.${var.domain}"
-#   validation_method = "DNS"
-# }
-
-data "aws_acm_certificate" "woodnet" {
-  domain   = "*.${var.domain}"
-  statuses = ["ISSUED"]
+resource "aws_acm_certificate" "woodnet" {
+  domain_name       = "*.${var.domain}"
+  validation_method = "DNS"
 }
 
-# resource "aws_route53_record" "woodnet_certificate" {
-#   for_each = {
-#     for dvo in data.aws_acm_certificate.woodnet.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+# data "aws_acm_certificate" "woodnet" {
+#   provider = aws.main
 
-#   # allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 300
-#   type            = each.value.type
-#   zone_id         = data.aws_route53_zone.main.zone_id
+#   domain   = "*.${var.domain}"
+#   statuses = ["ISSUED"]
 # }
 
-# resource "aws_acm_certificate_validation" "this" {
-#   certificate_arn         = data.aws_acm_certificate.woodnet.arn
-#   validation_record_fqdns = [for record in aws_route53_record.woodnet_certificate : record.fqdn]
-# }
+resource "aws_route53_record" "woodnet_certificate" {
+  provider = aws.main
+
+  for_each = {
+    for dvo in data.aws_acm_certificate.woodnet.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  # allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 300
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.main.zone_id
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn         = data.aws_acm_certificate.woodnet.arn
+  validation_record_fqdns = [for record in aws_route53_record.woodnet_certificate : record.fqdn]
+}
