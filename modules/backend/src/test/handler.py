d28d2af199e70d2379458b1aec5479a2af64d9d4
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-03-16 01:24:19 -0400

    sorting out backend stuff

--- modules/backend/src/test/handler.py
+++ modules/backend/src/test/handler.py
@@ -1,7 +1,11 @@
+import json
 import logging 
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    print("Hello world")
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
