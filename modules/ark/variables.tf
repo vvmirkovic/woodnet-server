485381cd21a989e3fba9c9e70c37e8cc60023a85
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-03-16 00:05:17 -0400

    adding some route53 stuff before fixing backend

--- modules/ark/variables.tf
+++ modules/ark/variables.tf
@@ -1,3 +1,5 @@
+variable "domain" {}
+variable "subdomain" {}
 variable "server_image" {}
 variable "subnet_group_id" {}
 variable "vpc_id" {}
--- modules/backend/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "domain" {}
-
-variable "subdomain" {}
