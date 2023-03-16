1864be6f413b045ddbfbabc965445185c36e3c2c
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-03-15 23:14:31 -0400

    backend api

--- modules/backend/api.tf
+++ modules/backend/api.tf
@@ -1,4 +1,4 @@
-resource "aws_api_gateway_rest_api" "minecraft" {
+resource "aws_api_gateway_rest_api" "woodnet" {
   body = file("${path.module}/api.json")
 
   name = "woodnet-backend"
@@ -6,4 +6,22 @@ resource "aws_api_gateway_rest_api" "minecraft" {
   endpoint_configuration {
     types = ["REGIONAL"]
   }
+}
+
+resource "aws_api_gateway_deployment" "woodnet" {
+  rest_api_id = aws_api_gateway_rest_api.woodnet.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.woodnet.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "woodnet" {
+  deployment_id = aws_api_gateway_deployment.woodnet.id
+  rest_api_id   = aws_api_gateway_rest_api.woodnet.id
+  stage_name    = "woodnet"
 }
