6efb8f08434a60409ef33124ce6afb2b2578bc91
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-03-16 23:22:24 -0400

    always run local-exec

--- provisioners.tf
+++ provisioners.tf
@@ -2,4 +2,7 @@ resource "null_resource" "backend_url_env" {
   provisioner "local-exec" {
     command = "echo \"${module.backend.backend_url}\" > backend_url.txt"
   }
+  triggers = {
+    always_run = timestamp()
+  }
 }
