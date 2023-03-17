e7b2a7511269ca9280347f1f583d09c0425ed578
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-03-16 21:47:10 -0400

    outputs, env vars, reworking workflow

--- provisioners.tf
+++ provisioners.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "backend_url_env" {
-    provisioner "local-exec" {
-        command = "export BACKEND_URL=${module.backend.backend_url}"
-    }
+  provisioner "local-exec" {
+    command = "export BACKEND_URL=${module.backend.backend_url}"
+  }
 }
