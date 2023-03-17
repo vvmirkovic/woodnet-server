0618ec49be1783330b68fcc3a6a7d7605fdc95da
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-03-16 23:54:16 -0400

    updates

--- modules/backend/iam.tf
+++ modules/backend/iam.tf
@@ -2,40 +2,11 @@ resource "aws_iam_role" "lambda_execution" {
   name = "woodnet_lambda"
   managed_policy_arns = [aws_iam_policy.lambda_policy.arn]
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
+  assume_role_policy = file("${path.module}/policies/lambda_trust.json")
 }
 
 resource "aws_iam_policy" "lambda_policy" {
   name = "woodnet_lambda"
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": [
-                "arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/test:*"
-            ]
-        }
-    ]
-}
-  EOF
+  policy = file("${path.module}/policies/lambda_execution.json")
 }
