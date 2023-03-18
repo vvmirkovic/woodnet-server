e510ad3cc82b2f9b94fbed5f18d9118dbd51ec48
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-03-17 20:54:44 -0400

    switching to ev2

---
+++ modules/ark/asg.tf
@@ -0,0 +1,43 @@
+resource "aws_autoscaling_group" "ark" {
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 0
+
+  launch_template {
+    id      = aws_launch_template.ark.id
+    version = "$Latest"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      desired_capacity
+    ]
+  }
+}
+
+resource "aws_launch_template" "ark" {
+  name = "ark"
+  instance_profile = "AmazonEC2RoleforSSMRole"
+  image_id = aws_ami.ecs_optimized.id
+  instance_type = "t3.medium"
+
+  # network_interfaces {
+  #   associate_public_ip_address = false
+  # }
+
+  user_data = <<EOF
+#!/bin/bash
+echo "ECS_CLUSTER=MyCluster" >> /etc/ecs/ecs.config
+EOF
+
+}
+
+data "aws_ami" "ecs_optimized" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-ecs-hvm-2.0.20*-x86_64-ebs"]
+  }
+}
--- modules/ark/ecs.tf
+++ modules/ark/ecs.tf
@@ -28,7 +28,7 @@ resource "aws_efs_mount_target" "ark-backups" {
 
 
 
-resource "aws_ecs_task_definition" "ark" {
+/*resource "aws_ecs_task_definition" "ark" {
   family                   = "ark"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
@@ -139,6 +139,80 @@ resource "aws_ecs_task_definition" "ark" {
   #   expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
   # }
 }
+*/
+resource "aws_ecs_task_definition" "ark" {
+  family                   = "ark"
+  network_mode             = "awsvpc"
+  execution_role_arn       = aws_iam_role.ark_server.arn
+  container_definitions = jsonencode([
+    {
+      name   = "ark-server"
+      image  = var.server_image
+      # entryPoint: ["/"],
+      environment = local.ark_environment_variables
+      essential   = true
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          "awslogs-group" : "/ark",
+          "awslogs-region" : "us-east-1",
+          "awslogs-create-group" : "true",
+          "awslogs-stream-prefix" : "ark"
+        }
+      }
+      portMappings = [
+        {
+          containerPort = local.port_game_client
+          hostPort      = local.port_game_client
+          protocol      = "udp"
+        },
+        {
+          containerPort = local.port_raw_udp
+          hostPort      = local.port_raw_udp
+          protocol      = "udp"
+        },
+        {
+          containerPort = local.port_rcon_management
+          hostPort      = local.port_rcon_management
+          protocol      = "tcp"
+        },
+        {
+          containerPort = local.port_server_list
+          hostPort      = local.port_server_list
+          protocol      = "udp"
+        }
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "ark-server",
+          containerPath = "/app",
+          readOnly      = false
+        },
+        {
+          sourceVolume  = "ark-server-backups",
+          containerPath = "/home/steam/ARK-Backups",
+          readOnly      = false
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name = "ark-server"
+
+    efs_volume_configuration {
+      file_system_id = aws_efs_file_system.ark-server.id
+    }
+  }
+
+  volume {
+    name = "ark-server-backups"
+
+    efs_volume_configuration {
+      file_system_id = aws_efs_file_system.ark-server-backups.id
+    }
+  }
+}
 
 resource "aws_ecs_cluster" "ark" {
   name = "ark"
@@ -149,12 +223,53 @@ resource "aws_ecs_cluster" "ark" {
   }
 }
 
+resource "aws_ecs_capacity_provider" "ark" {
+  name = "ark"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.ark.arn
+    managed_termination_protection = "ENABLED"
+
+    # managed_scaling {
+    #   maximum_scaling_step_size = 1
+    #   minimum_scaling_step_size = 1
+    #   status                    = "ENABLED"
+    #   target_capacity           = 1
+    # }
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "ark" {
+  cluster_name = aws_ecs_cluster.ark.name
+
+  capacity_providers = [aws_ecs_capacity_provider.ark.name]
+}
+
+# resource "aws_ecs_service" "ark" {
+#   name            = "ark"
+#   cluster         = aws_ecs_cluster.ark.id
+#   launch_type     = "FARGATE"
+#   task_definition = aws_ecs_task_definition.ark.arn
+#   desired_count   = 1
+
+#   network_configuration {
+#     subnets          = [var.subnet_group_id]
+#     security_groups  = [aws_security_group.ark_server.id]
+#     assign_public_ip = true
+#   }
+# }
+
 resource "aws_ecs_service" "ark" {
   name            = "ark"
   cluster         = aws_ecs_cluster.ark.id
-  launch_type     = "FARGATE"
+  launch_type     = "EC2"
+  scheduling_stategy = "DAEMON"
   task_definition = aws_ecs_task_definition.ark.arn
-  desired_count   = 1
+
+  capacity_provider_strategy {
+    capacity_provider = 
+    weight = 100
+  }
 
   network_configuration {
     subnets          = [var.subnet_group_id]
