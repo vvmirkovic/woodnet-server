05c40364b41a5bbb286f06a8e205f69f8271fe5a
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-03-17 23:16:32 -0400

    ecs permissions

--- modules/ark/iam.tf
+++ modules/ark/iam.tf
@@ -73,5 +73,5 @@ resource "aws_iam_role_policy_attachment" "ecs_ssm" {
 
 resource "aws_iam_role_policy_attachment" "ecs_ecs" {
   role       = aws_iam_role.ecs_instance.name
-  policy_arn = data.aws_iam_policy.ecs
+  policy_arn = data.aws_iam_policy.ecs.arn
 }
