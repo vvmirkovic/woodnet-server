2e5324b5dbe1bdd32518aab090eacf5deebf0ea3
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-03-17 21:03:18 -0400

    switching to ec2 for ark

--- modules/ark/asg.tf
+++ modules/ark/asg.tf
@@ -17,7 +17,7 @@ resource "aws_autoscaling_group" "ark" {
 
 resource "aws_launch_template" "ark" {
   name = "ark"
-  instance_profile = "AmazonEC2RoleforSSMRole"
+  iam_instance_profile = "AmazonEC2RoleforSSMRole"
   image_id = data.aws_ami.ecs_optimized.id
   instance_type = "t3.medium"
 
