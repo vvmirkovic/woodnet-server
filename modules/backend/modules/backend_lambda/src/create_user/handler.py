30cf55bc31919ec922047e5c80dfa1a57cba4c9e
vlad <vdmirkovic@gmail.com>
2023-03-26 00:58:55 -0400

    debugging lambda

--- modules/backend/modules/backend_lambda/src/create_user/handler.py
+++ modules/backend/modules/backend_lambda/src/create_user/handler.py
@@ -1,7 +1,10 @@
-import logging 
 import boto3
+import json
+import logging
+import random
 import re
 import string
+from botocore.exceptions import ClientError
 from os import environ
 
 logger = logging.getLogger()
@@ -10,29 +13,27 @@ logger.setLevel(logging.INFO)
 USER_POOL_ID = environ["USER_POOL_ID"]
 
 def validate_password(password):
-    has_upper = bool(re.search(r'[A-Z]', name))
-    has_lower = bool(re.search(r'[a-z]', name))
-    has_digit = bool(re.search(r'[0-9]', name))
-    has_special = bool(re.search(r'[$*.{}()?-"!@#%&,><:;|_~+=]', name))
+    has_upper = bool(re.search(r'[A-Z]', password))
+    has_lower = bool(re.search(r'[a-z]', password))
+    has_digit = bool(re.search(r'[0-9]', password))
+    has_special = bool(re.search(r'[$*.{}()"!@#%&,><:;|_~+=]', password))
     valid = has_upper and has_lower and has_digit and has_special
 
     return valid 
 
 def generate_password():
-    letters = string.ascii_uppercase + string.digits 
-    special_characters = '$*.{}()?-"!@#%&,><:;|_~+='
-    characters = letters + special_characters
+    
+    characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + '$*.{}()?-"!@#%&,><:;|_~+='
 
     while True:
         password = ''.join(random.choice(characters) for _ in range(12))
     
-        if validate(password):
+        if validate_password(password):
             return password
 
 
 
 def lambda_handler(event, context):
-    CognitoIdentityProvider.Client.admin_create_user
 
     if 'username' not in event:
         return {
@@ -43,16 +44,25 @@ def lambda_handler(event, context):
     username = event['username']
     temp_password = generate_password()
 
-    client = boto3.client('cognito')
-    client.admin_create_user(
-        UserPoolId = USER_POOL_ID
-        Username = username
-        TemporaryPassword = temp_password
-    )
+    client = boto3.client('cognito-idp')
+    
+    try:
+        client.admin_create_user(
+            UserPoolId = USER_POOL_ID,
+            Username = username,
+            TemporaryPassword = temp_password
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'UsernameExistsException':
+            return {
+                'statusCode': 400,
+                'body': json.dumps(f'Invalid request. Username already exists')
+            }
         
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'password': password
-            'message': f'{username} added to users')
+            'password': temp_password,
+            'message': f'{username} added to users'
+        })
     }
