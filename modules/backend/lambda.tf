0d8bf963e736fe208623ab6a7ee29052ba8a5918
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-04-05 22:49:26 -0400

    moving locals

--- modules/backend/lambda.tf
+++ modules/backend/lambda.tf
@@ -1,14 +1,3 @@
-locals {
-  test_path      = "${path.module}/src/test"
-  start_ark_path = "${path.module}/src/start_ark"
-
-  server_subdomain_base = "ark"
-  env_modifier          = var.env == "prod" ? "" : "${var.env}."
-  server_subdomain      = "${local.env_modifier}${local.server_subdomain_base}."
-  record_name           = "${local.server_subdomain}${data.aws_route53_zone.main.name}"
-}
-
-
 module "test_lambda" {
   source = "./modules/backend_lambda"
 
--- modules/backend/locals.tf
+++ modules/backend/locals.tf
@@ -1,4 +1,11 @@
 locals {
-  env_modifier = var.env == "prod" ? "" : "${var.env}."
+  test_path      = "${path.module}/src/test"
+  start_ark_path = "${path.module}/src/start_ark"
+
+  server_subdomain_base = "ark"
+  env_modifier          = var.env == "prod" ? "" : "${var.env}."
+  server_subdomain      = "${local.env_modifier}${local.server_subdomain_base}."
+  record_name           = "${local.server_subdomain}${data.aws_route53_zone.main.name}"
+  
   backend_domain = "backend.${local.env_modifier}${var.domain}"
 }
