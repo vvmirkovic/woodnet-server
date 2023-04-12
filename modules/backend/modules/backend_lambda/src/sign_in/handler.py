d77a348a15919f7a690d2d0ac2f85328f1fd362b
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-04-11 22:38:15 -0400

    using general response

--- modules/backend/modules/backend_lambda/src/sign_in/handler.py
+++ modules/backend/modules/backend_lambda/src/sign_in/handler.py
@@ -3,7 +3,7 @@ import json
 import logging
 from botocore.exceptions import ClientError
 from os import environ
-from backend_handler import success
+from backend_handler import response
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,10 +14,15 @@ def lambda_handler(event, context):
 
     body = json.loads(event['body'])
     if 'username' not in body or 'password' not in body:
-        return {
-            'statusCode': 400,
-            'body': json.dumps(f'Invalid request. Must provide username and password')
-        }
+        return response(
+                event,
+                400,
+                json.dumps(f'Invalid request. Must provide username and password')
+            )
+        # {
+        #     'statusCode': 400,
+        #     'body': json.dumps(f'Invalid request. Must provide username and password')
+        # }
     
     username = body['username']
     password = body['password']
@@ -35,15 +40,20 @@ def lambda_handler(event, context):
         )
     except ClientError as e:
         if e.response['Error']['Code'] == 'NotAuthorizedException':
-            return {
-                'statusCode': 400,
-                'headers': {
-                    "Access-Control-Allow-Headers" : "Content-Type",
-                    "Access-Control-Allow-Origin": "https://dev.woodnet.io",
-                    "Access-Control-Allow-Methods": "OPTIONS,POST"
-                },
-                'body': json.dumps(f'Invalid username and password.')
-            }
+            return response(
+                event,
+                400,
+                json.dumps(f'Invalid username and password.')
+            )
+        # {
+        #         'statusCode': 400,
+        #         'headers': {
+        #             "Access-Control-Allow-Headers" : "Content-Type",
+        #             "Access-Control-Allow-Origin": "https://dev.woodnet.io",
+        #             "Access-Control-Allow-Methods": "OPTIONS,POST"
+        #         },
+        #         'body': json.dumps(f'Invalid username and password.')
+        #     }
         else:
             raise
     
@@ -52,21 +62,10 @@ def lambda_handler(event, context):
     #         statusCode': 301
     #     }
     
-    return success(
+    return response(
         event,
         200,
         json.dumps({
             'message': response_init['AuthenticationResult']
         })
     )
-    return {
-        'statusCode': 200,
-        'headers': {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "https://dev.woodnet.io",
-            "Access-Control-Allow-Methods": "OPTIONS,POST"
-        },
-        'body': json.dumps({
-            'message': response_init['AuthenticationResult']
-        })
-    }
