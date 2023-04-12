9ea082ab12dab61b45dd6e0c9e904dfc0891e665
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-04-11 21:51:16 -0400

    fixing layer structure

--- modules/backend/lambda.tf
+++ modules/backend/lambda.tf
@@ -1,8 +1,8 @@
 # Create layers
 locals {
-  backend_handler_folder = "${path.module}/modules/backend_lambda/src/backend_handler/python/"
-  backend_handler_template = "${local.backend_handler_folder}backend_handler.py.tftpl"
-  backend_handler_dest = "${local.backend_handler_folder}backend_handler.py"
+  backend_handler_folder = "${path.module}/modules/backend_lambda/src/backend_handler/"
+  backend_handler_template = "${local.backend_handler_folder}python/backend_handler.py.tftpl"
+  backend_handler_dest = "${local.backend_handler_folder}python/backend_handler.py"
 }
 
 resource "local_file" "foo" {
