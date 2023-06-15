16a99b7a6aa8ffa3be8f76617db9e7f9d75b3b2e
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-06-14 21:18:21 -0400

    adding subdomain to cert

--- modules/frontend/route53.tf
+++ modules/frontend/route53.tf
@@ -2,7 +2,7 @@ locals {
   env_modifier       = var.env == "prod" ? "" : "${var.env}."
   subdomain_modifier = var.subdomain == "" ? "" : "${var.subdomain}."
   full_domain        = "${local.env_modifier}${local.subdomain_modifier}${var.domain}"
-  certificate        = var.env == "prod" && var.subdomain == "" ? var.domain : "*.${var.domain}"
+  certificate        = var.env == "prod" && var.subdomain == "" ? var.domain : "*.${local.subdomain_modifier}${var.domain}"
 }
 
 resource "aws_acm_certificate" "woodnet" {
