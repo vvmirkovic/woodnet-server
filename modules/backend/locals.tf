bc6e6acc0ad49388d3373a0e247eb413a95b741b
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-09-23 20:30:46 -0400

    adding variable for frontend subdomain info in backend module

--- modules/backend/locals.tf
+++ modules/backend/locals.tf
@@ -3,11 +3,11 @@ locals {
   start_ark_path = "${path.module}/src/start_ark"
 
   server_subdomain_base = "ark"
-  env_modifier          = var.env == "prod" ? "." : "${var.env}."
+  env_modifier          = var.env == "prod" ? "" : "${var.env}."
   server_subdomain      = "${local.env_modifier}${local.server_subdomain_base}."
   record_name           = "${local.server_subdomain}${data.aws_route53_zone.main.name}"
 
-  backend_domain              = "${local.env_modifier}backend.${var.domain}"
+  backend_domain  = "${local.env_modifier}backend.${var.domain}"
   frontend_subdomain_modifier = var.frontend_subdomain == "" ? "" : "${var.frontend_subdomain}."
-  frontend_domain             = "${local.env_modifier}${local.frontend_subdomain_modifier}${var.domain}"
+  frontend_domain = "${local.env_modifier}${local.frontend_subdomain_modifier}${var.domain}"
 }
