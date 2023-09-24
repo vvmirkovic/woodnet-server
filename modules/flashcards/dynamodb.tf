76aecaf74b3dbdc2b1fad3494735697ec4bfe6b2
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-09-24 13:17:04 -0400

    wmessing around with attributes

--- modules/flashcards/dynamodb.tf
+++ modules/flashcards/dynamodb.tf
@@ -4,7 +4,6 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "WordId"
-  range_key      = "WordId"
 
   attribute {
     name = "WordId"
