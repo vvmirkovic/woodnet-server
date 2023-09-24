0ad0c2aaa228b456c0ae9cda8358baab79d5a483
Vladimir Mirkovic <vdmirkovic@gmail.com>
2023-09-24 13:09:13 -0400

    wmessing around with attributes

--- modules/flashcards/dynamodb.tf
+++ modules/flashcards/dynamodb.tf
@@ -10,14 +10,4 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     name = "WordId"
     type = "N"
   }
-
-  attribute {
-    name = "Croatian"
-    type = "S"
-  }
-
-  attribute {
-    name = "English"
-    type = "S"
-  }
 }
